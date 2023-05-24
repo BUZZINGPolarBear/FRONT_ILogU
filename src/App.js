4f8b984f8fac1a0a46553284120fb7b4ca0637fe
Jeonjunhowi <jjh63360@kau.kr>
2023-05-24 16:43:16 +0900

    add: main screen bottom nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import styled from "styled-components";
+import "./App.css";
+import {
+	RecoilRoot,
+	atom,
+	selector,
+	useRecoilState,
+	useRecoilValue,
+} from "recoil";
+import * as recoilMain from "./recoil/recoil.App";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const MainScreen = styled.div`
+		position: fixed;
+		top: 0;
+		width: 100%;
+		height: 90vh;
+
+		background-color: #282c34;
+	`;
+	const BottomNavBar = styled.div`
+		position: fixed;
+		bottom: 0;
+		width: 100%;
+		height: 8vh;
+		padding-top: 10px;
+
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+		background-color: white;
+	`;
+	const NavImg = styled.div`
+		width: 20%;
+		height: 100%;
+	`;
+
+	const handleNavClick = (e, type) => {
+		e.preventDefault();
+		setNavigateBtn(type);
+	};
+	const navigateReoilSelectState = useRecoilValue(recoilMain.navigateRecoil);
+	const [navigateBtn, setNavigateBtn] = useRecoilState(
+		recoilMain.navigateRecoil
+	);
+	return (
+		<div className="App">
+			<MainScreen></MainScreen>
+			<BottomNavBar>
+				<NavImg onClick={(e) => handleNavClick(e, "home")}>
+					{navigateReoilSelectState === "home" ? (
+						<img src="/bottomNavBar/home_clicked.svg"></img>
+					) : (
+						<img src="/bottomNavBar/home_unclicked.svg"></img>
+					)}
+				</NavImg>
+				<NavImg onClick={(e) => handleNavClick(e, "finance")}>
+					{navigateReoilSelectState === "finance" ? (
+						<img src="/bottomNavBar/finance_clicked.svg"></img>
+					) : (
+						<img src="/bottomNavBar/finance_unclicked.svg"></img>
+					)}
+				</NavImg>
+				<NavImg onClick={(e) => handleNavClick(e, "feed")}>
+					{navigateReoilSelectState === "feed" ? (
+						<img src="/bottomNavBar/feed_clicked.svg"></img>
+					) : (
+						<img src="/bottomNavBar/feed_unclicked.svg"></img>
+					)}
+				</NavImg>
+				<NavImg onClick={(e) => handleNavClick(e, "challenge")}>
+					{navigateReoilSelectState === "challenge" ? (
+						<img src="/bottomNavBar/challenge_clicked.svg"></img>
+					) : (
+						<img src="/bottomNavBar/challenge_unclicked.svg"></img>
+					)}
+				</NavImg>
+				<NavImg onClick={(e) => handleNavClick(e, "mypage")}>
+					{navigateReoilSelectState === "mypage" ? (
+						<img src="/bottomNavBar/mypage_clicked.svg"></img>
+					) : (
+						<img src="/bottomNavBar/mypage_unclicked.svg"></img>
+					)}
+				</NavImg>
+			</BottomNavBar>
+		</div>
+	);
 }
 
 export default App;
