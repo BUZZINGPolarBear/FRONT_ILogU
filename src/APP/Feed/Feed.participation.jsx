7b5478a177cd4a93346317a0338d79217a40d174
Jeonjunhowi <jjh63360@kau.kr>
2023-06-02 16:25:02 +0900

    add: 피드 글 프로필 부분 추가

--- src/APP/Feed/Feed.participation.jsx
+++ src/APP/Feed/Feed.participation.jsx
@@ -8,12 +8,30 @@ import {
 	Routes,
 	useNavigate,
 } from 'react-router-dom';
-import * as FeedMainS from './Styles/Feed.main.styles';
 import * as tokens from '../../tokens';
+import * as FeedMainS from './Styles/Feed.main.styles';
+import * as FeedparicipateS from './Styles/Feed.participation.styles';
+
 import FeedAll from './Feed.all';
 
 function FeedParticipation() {
-	return <>Participation Area</>;
+	return (
+		<>
+			<FeedparicipateS.FeedChallengeWrapper>
+				<FeedparicipateS.FeedChallengeUserWrapper>
+					<FeedparicipateS.FeedChallengeUserImage picUrl="/Feed/userPic.jpg"></FeedparicipateS.FeedChallengeUserImage>
+					<FeedparicipateS.FeedChallengeUserInfoWrapper>
+						<FeedparicipateS.FeedChallengeUserInfo>
+							준휘대디
+						</FeedparicipateS.FeedChallengeUserInfo>
+						<FeedparicipateS.FeedChallengeUserInfoDate>
+							2023.06.02
+						</FeedparicipateS.FeedChallengeUserInfoDate>
+					</FeedparicipateS.FeedChallengeUserInfoWrapper>
+				</FeedparicipateS.FeedChallengeUserWrapper>
+			</FeedparicipateS.FeedChallengeWrapper>
+		</>
+	);
 }
 
 export default FeedParticipation;
---
+++ src/APP/Feed/Styles/Feed.participation.styles.js
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+import * as tokens from '../../../tokens';
+
+const defaultProfilePicUrl = '/Feed/Default_pfp.png';
+export const FeedChallengeWrapper = styled.div`
+	width: 95%;
+	height: 50vh;
+	margin: auto;
+	margin-bottom: 3vh;
+
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+
+	// background-color: antiquewhite;
+`;
+
+export const FeedChallengeUserWrapper = styled.div`
+	width: 60%;
+	height: 15%;
+	max-width: 720px;
+
+	display: flex;
+	align-items: center;
+	justify-content: flex-start;
+
+	background-color: white;
+`;
+
+export const FeedChallengeUserImage = styled.div`
+	width: 60px;
+	height: 60px;
+	border-radius: 50%;
+	// background-color: ${tokens.colors.grey_100};
+
+	background-image: url(${(props) => props.picUrl || defaultProfilePicUrl});
+	background-size: cover;
+	background-position: center;
+
+	@media (max-width: 380px) {
+		width: 40px;
+		height: 40px;
+	}
+`;
+
+export const FeedChallengeUserInfoWrapper = styled.div`
+	width: fit-content;
+	height: 100%;
+	position: relative;
+
+	margin-left: 5%;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	justify-content: center;
+	text-align: left;
+`;
+
+export const FeedChallengeUserInfo = styled.span`
+	color: ${tokens.colors.grey_700};
+	${tokens.typography.body_3_SB};
+`;
+
+export const FeedChallengeUserInfoDate = styled.span`
+	color: ${tokens.colors.grey_200};
+	${tokens.typography.caption};
+`;
