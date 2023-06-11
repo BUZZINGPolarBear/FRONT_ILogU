9c67ac9c87fe0e6b8fe9efb5f9dd5deff85b284e
Jeonjunhowi <jjh63360@kau.kr>
2023-06-11 23:33:40 +0900

    fix: workflow

--- src/APP/Feed/writeFeed/APIs/uploadPhoto.js
+++ src/APP/Feed/writeFeed/APIs/uploadPhoto.js
@@ -14,11 +14,11 @@ export const autoGenerateFeed = async (file) => {
 			//2. 로컬 테스트시 주석 해제
 			//`${process.env.REACT_APP_ILOGU_LOCAL_API}/api/board/image/explain?prompt=이건 사진에서 추출한 키워드야. 이 키워드를 이용해서 사진을 SNS에 업로드하는 부모처럼 따뜻한 글과 여행과 관련된 해시태그를 1개만 한국어로 써줘.`,
 			formData,
-			// {
-			// 	headers: {
-			// 		'Content-Type': 'multipart/form-data',
-			// 	},
-			// },
+			{
+				headers: {
+					'Content-Type': 'multipart/form-data',
+				},
+			},
 		);
 		return axiosResponse.data;
 	} catch (error) {
