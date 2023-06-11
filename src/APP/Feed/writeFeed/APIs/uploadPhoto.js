c94c5581a898ac0fc797a8e67c4f8a0efab8a77d
Jeonjunhowi <jjh63360@kau.kr>
2023-06-11 22:31:38 +0900

    add: 사진 자동 분석 완료

--- src/APP/Feed/writeFeed/APIs/uploadPhoto.js
+++ src/APP/Feed/writeFeed/APIs/uploadPhoto.js
@@ -3,28 +3,24 @@ const FormData = require('form-data');
 
 export const autoGenerateFeed = async (file) => {
 	try {
-		fetch(file)
-			.then((response) => response.blob())
-			.then(async (blobData) => {
-				const formData = new FormData();
-				formData.append('file', blobData, 'image.jpg');
+		const response = await fetch(file);
+		const blobData = await response.blob();
+		const formData = new FormData();
+		formData.append('file', blobData, 'image.jpg');
 
-				const response = await axios.post(
-					//1. 배포 서버로 테스트시 주석 해제
-					//`${process.env.REACT_APP_ILOGU_API_SERVER}/api/board/image/explain?prompt=이건 사진에서 추출한 키워드야. 이 키워드를 이용해서 사진을 SNS에 업로드하는 부모처럼 따뜻한 글과 여행과 관련된 해시태그를 1개만 한국어로 써줘.`,
-					//2. 로컬 테스트시 주석 해제
-					`${process.env.REACT_APP_ILOGU_LOCAL_API}/api/board/image/explain?prompt=이건 사진에서 추출한 키워드야. 이 키워드를 이용해서 사진을 SNS에 업로드하는 부모처럼 따뜻한 글과 여행과 관련된 해시태그를 1개만 한국어로 써줘.`,
-					formData,
-					{
-						headers: {
-							'Content-Type': 'multipart/form-data',
-						},
-					},
-				);
-
-				console.log(response.data);
-				return response.data;
-			});
+		const axiosResponse = await axios.post(
+			//1. 배포 서버로 테스트시 주석 해제
+			`${process.env.REACT_APP_ILOGU_API_SERVER}/api/board/image/explain?prompt=이건 사진에서 추출한 키워드야. 이 키워드를 이용해서 사진을 SNS에 업로드하는 부모처럼 따뜻한 글과 여행과 관련된 해시태그를 1개만 한국어로 써줘.`,
+			//2. 로컬 테스트시 주석 해제
+			//`${process.env.REACT_APP_ILOGU_LOCAL_API}/api/board/image/explain?prompt=이건 사진에서 추출한 키워드야. 이 키워드를 이용해서 사진을 SNS에 업로드하는 부모처럼 따뜻한 글과 여행과 관련된 해시태그를 1개만 한국어로 써줘.`,
+			formData,
+			{
+				headers: {
+					'Content-Type': 'multipart/form-data',
+				},
+			},
+		);
+		return axiosResponse.data;
 	} catch (error) {
 		console.error(error);
 	}
