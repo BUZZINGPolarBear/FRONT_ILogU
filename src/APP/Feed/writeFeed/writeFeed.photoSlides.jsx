6b07f8e8fc8dec41c418e5526c9ac68cc4a12663
Jeonjunhowi <jjh63360@kau.kr>
2023-06-12 15:46:18 +0900

    fix: 사진 찌그러지던 문제 해결

--- src/APP/Feed/writeFeed/writeFeed.photoSlides.jsx
+++ src/APP/Feed/writeFeed/writeFeed.photoSlides.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import * as photoStyle from './Styles/uploadPhoto.styles';
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
 
 function PhotoSlider(props) {
 	const selectedImages = props.imageArr;
@@ -20,17 +27,23 @@ function PhotoSlider(props) {
 	};
 
 	return (
-		<Slider {...settings} className="carousel-container">
+		<photoStyle.StyledSwiper spaceBetween={50} slidesPerView={1}>
 			{photos.map((photo) => (
-				<div key={photo.id}>
+				<photoStyle.StyledSwiperSlide key={photo.id}>
 					<img
 						src={photo.url}
-						style={{ width: '365px', height: '245px', borderRadius: '10px' }}
+						style={{
+							width: '365px',
+							height: '245px',
+							borderRadius: '10px',
+							objectFit: 'cover',
+							objectPosition: '50% 50%',
+						}}
 						alt={`Photo ${photo.id}`}
 					/>
-				</div>
+				</photoStyle.StyledSwiperSlide>
 			))}
-		</Slider>
+		</photoStyle.StyledSwiper>
 	);
 }
 
