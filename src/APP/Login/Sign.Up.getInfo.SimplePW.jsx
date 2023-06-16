77df69c5d0e45a09d5156bf04cbcccb20264b061
Jeonjunhowi <jjh63360@kau.kr>
2023-06-16 23:43:34 +0900

    add: simple password 입력 틀

--- src/APP/Login/Sign.Up.getInfo.SimplePW.jsx
+++ src/APP/Login/Sign.Up.getInfo.SimplePW.jsx
@@ -13,7 +13,12 @@ import * as signUpS from './styles/Sign.Up.Main.Styles';
 import * as signInRecoil from './recoil/Login.recoil.states';
 
 function SimplePassWord() {
-	const navigate = useNavigate();
+	const [password, setPassword] = useState('');
+	const maxLength = 6;
+
+	const handleChange = (event) => {
+		setPassword(event.target.value);
+	};
 
 	const [isNextBtnClicked, setIsNextBtnClicked] = useRecoilState(
 		signInRecoil.isTermsOfAgreeBtnClicked,
@@ -35,12 +40,32 @@ function SimplePassWord() {
 					</signUpS.MainSubTitle>
 				</signUpS.MainTitleWrapper>
 
-				<signUpS.MainContentWrapper></signUpS.MainContentWrapper>
+				<signUpS.MainContentWrapper
+					style={{
+						display: 'flex',
+						alignItems: 'flex-start',
+						padding: '10% 5%',
+						justifyContent: 'center',
+					}}
+				>
+					<input
+						type="number"
+						value={password}
+						onChange={handleChange}
+						maxLength={maxLength}
+						// style={{ display: 'none' }} // Hides the actual input
+					/>
+					<signUpS.SimplePwWrapper>
+						{[...Array(maxLength)].map((_, i) => (
+							<signUpS.PasswordCircle key={i} filled={i < password.length} />
+						))}
+					</signUpS.SimplePwWrapper>
+				</signUpS.MainContentWrapper>
 				<signUpS.BtnWrapper
 					is_btn_available={false}
 					onClick={(e) => handleSubmitBtnClick(e)}
 				>
-					약관에 동의합니다
+					완료
 				</signUpS.BtnWrapper>
 			</signUpS.SignUpMain>
 		</>
