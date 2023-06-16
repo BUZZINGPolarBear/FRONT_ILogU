ff1e051ad1cb452753910c663160f7fff861d2f4
Jeonjunhowi <jjh63360@kau.kr>
2023-06-16 23:43:59 +0900

    fix: hide input box

--- src/APP/Login/Sign.Up.getInfo.SimplePW.jsx
+++ src/APP/Login/Sign.Up.getInfo.SimplePW.jsx
@@ -53,7 +53,7 @@ function SimplePassWord() {
 						value={password}
 						onChange={handleChange}
 						maxLength={maxLength}
-						// style={{ display: 'none' }} // Hides the actual input
+						style={{ display: 'none' }} // Hides the actual input
 					/>
 					<signUpS.SimplePwWrapper>
 						{[...Array(maxLength)].map((_, i) => (
