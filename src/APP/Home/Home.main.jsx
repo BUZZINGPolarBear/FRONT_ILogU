887a7e44ac212dc41a81926bacc2c48e9b87334b
Jeonjunhowi <jjh63360@kau.kr>
2023-06-25 20:25:42 +0900

    add: 홈화면 상단 틀

--- src/APP/Home/Home.main.jsx
+++ src/APP/Home/Home.main.jsx
@@ -1,15 +1,61 @@
+import React, { useEffect, useState, useRef } from 'react';
+
 import * as HomeS from './Styles/Home.main.styles';
 import * as tokens from '../../tokens';
 
 function HomeMain() {
+	const [mainCategory, setMainCategory] = useState('investment');
+
+	const handleMainCategory = (e, type) => {
+		e.preventDefault();
+		setMainCategory(type);
+	};
 	return (
 		<HomeS.HomeMainScreen>
 			<HomeS.HomeMainScreenHeader>
 				<HomeS.HomeMainHeaderContent>
-					<HomeS.HomeMainHeaderContentTitle></HomeS.HomeMainHeaderContentTitle>
-					<HomeS.HomeMainHeaderContentTitle></HomeS.HomeMainHeaderContentTitle>
+					<HomeS.HomeMainHeaderContentTitle>
+						<HomeS.HomeMainHeaderTitle type="sub">
+							하나네 가족은
+						</HomeS.HomeMainHeaderTitle>
+						<HomeS.HomeMainHeaderTitle type="main">
+							프로여행러
+						</HomeS.HomeMainHeaderTitle>
+					</HomeS.HomeMainHeaderContentTitle>
+					<HomeS.HomeMainHeaderContentTitle
+						style={{ width: '30%', alignItems: 'center' }}
+					>
+						<HomeS.HomeMainHeaderCircleIcon></HomeS.HomeMainHeaderCircleIcon>
+					</HomeS.HomeMainHeaderContentTitle>
 				</HomeS.HomeMainHeaderContent>
 			</HomeS.HomeMainScreenHeader>
+
+			<HomeS.HomeMainSelectWrapper>
+				<HomeS.HomeMainCategoryBtn
+					isSelected={mainCategory == 'investment' ? true : false}
+					onClick={(e) => {
+						handleMainCategory(e, 'investment');
+					}}
+				>
+					투자상품 추천
+				</HomeS.HomeMainCategoryBtn>
+				<HomeS.HomeMainCategoryBtn
+					isSelected={mainCategory == 'insurence' ? true : false}
+					onClick={(e) => {
+						handleMainCategory(e, 'insurence');
+					}}
+				>
+					보험 상품
+				</HomeS.HomeMainCategoryBtn>
+				<HomeS.HomeMainCategoryBtn
+					isSelected={mainCategory == 'deposit' ? true : false}
+					onClick={(e) => {
+						handleMainCategory(e, 'deposit');
+					}}
+				>
+					예적금 상품
+				</HomeS.HomeMainCategoryBtn>
+			</HomeS.HomeMainSelectWrapper>
 		</HomeS.HomeMainScreen>
 	);
 }
--- src/APP/Home/Styles/Home.main.styles.js
+++ src/APP/Home/Styles/Home.main.styles.js
@@ -12,7 +12,7 @@ export const HomeMainScreen = styled.div`
 
 export const HomeMainScreenHeader = styled.div`
 	width: 100%;
-	height: 15vh;
+	height: 12vh;
 	margin-top: 10px;
 	min-height: 120px;
 
@@ -31,7 +31,7 @@ export const HomeMainHeaderContent = styled.div`
 	align-items: center;
 	justify-content: space-between;
 
-	padding: 0 15px;
+	padding: 0 25px;
 	border-radius: 16px;
 	background-color: rgba(29, 203, 157, 0.1);
 `;
@@ -42,9 +42,10 @@ export const HomeMainHeaderContentTitle = styled.div`
 
 	display: flex;
 	flex-direction: column;
-	justify-content: space-around;
+	align-items: flex-start;
+	justify-content: center;
 
-	background-color: antiquewhite;
+	// background-color: antiquewhite;
 `;
 
 export const HomeMainHeaderTitle = styled.span`
@@ -53,4 +54,47 @@ export const HomeMainHeaderTitle = styled.span`
 		props.type === 'sub'
 			? tokens.typography.body_2
 			: tokens.typography.headline_5};
+`;
+
+export const HomeMainHeaderCircleIcon = styled.div`
+	width: 65px;
+	height: 65px;
+	border-radius: 50%;
+
+	background-image: url('/mainScreen/car_icon.svg');
+	background-size: 40px 40px;
+	background-position: center;
+	background-repeat: no-repeat;
+
+	background-color: #ffffff;
+
+	box-shadow: 0px 0px 10px 5px rgba(0, 0, 0, 0.05);
+`;
+
+export const HomeMainSelectWrapper = styled.div`
+	width: 100%;
+	height: 60px;
+	padding: 0 10%;
+
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	align-items: flex-end;
+	justify-content: space-around;
+
+	border-bottom: 1px solid ${tokens.colors.grey_50};
+`;
+
+export const HomeMainCategoryBtn = styled.div`
+	width: 30%;
+	height: 35px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	${tokens.typography.subtitle_3};
+
+	${(props) =>
+		props.isSelected
+			? `color:${tokens.colors.green_500}; border-bottom: 2px solid ${tokens.colors.green_500};`
+			: `color:${tokens.colors.grey_100};`}
 `;
