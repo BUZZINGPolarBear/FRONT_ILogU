286ba8361874e09d3f6c8d497ce9b4cfaf32e3ff
Jeonjunhowi <jjh63360@kau.kr>
2023-06-27 09:21:15 +0900

    add: 맞춤보험 추천 카드 추가

--- src/APP/Home/Home.insurence.jsx
+++ src/APP/Home/Home.insurence.jsx
@@ -12,6 +12,56 @@ function InsurenceComponent() {
 		const fetchInsurence = async () => {
 			let fetchResponse = await InsurenceApi.getInsurence();
 			console.log(fetchResponse.result);
+			let localInsurenceDiv = [];
+			for (let i = 0; i < fetchResponse.result.length; i++) {
+				const localInsurence = fetchResponse.result[i];
+				if (localInsurence.id == 19) {
+					localInsurenceDiv.push(
+						<HomeS.MainRecommendContentWrapper>
+							<InsurenceS.InsurenceRecommendImage>
+								<InsurenceS.InsurenceImage image="car"></InsurenceS.InsurenceImage>
+							</InsurenceS.InsurenceRecommendImage>
+							<InsurenceS.InsurenceRecommendText>
+								엔젤꿈나무사랑보험
+							</InsurenceS.InsurenceRecommendText>
+							<InsurenceS.InsurenceRecommendImage>
+								<InsurenceS.InsurenceImage image="detailView"></InsurenceS.InsurenceImage>
+							</InsurenceS.InsurenceRecommendImage>
+						</HomeS.MainRecommendContentWrapper>,
+					);
+				}
+				if (localInsurence.id == 20) {
+					localInsurenceDiv.push(
+						<HomeS.MainRecommendContentWrapper>
+							<InsurenceS.InsurenceRecommendImage>
+								<InsurenceS.InsurenceImage image="travel"></InsurenceS.InsurenceImage>
+							</InsurenceS.InsurenceRecommendImage>
+							<InsurenceS.InsurenceRecommendText>
+								e우리아이보장보험
+							</InsurenceS.InsurenceRecommendText>
+							<InsurenceS.InsurenceRecommendImage>
+								<InsurenceS.InsurenceImage image="detailView"></InsurenceS.InsurenceImage>
+							</InsurenceS.InsurenceRecommendImage>
+						</HomeS.MainRecommendContentWrapper>,
+					);
+				}
+				if (localInsurence.id == 21) {
+					localInsurenceDiv.push(
+						<HomeS.MainRecommendContentWrapper>
+							<InsurenceS.InsurenceRecommendImage>
+								<InsurenceS.InsurenceImage image="travel"></InsurenceS.InsurenceImage>
+							</InsurenceS.InsurenceRecommendImage>
+							<InsurenceS.InsurenceRecommendText>
+								NH운전자상해보험
+							</InsurenceS.InsurenceRecommendText>
+							<InsurenceS.InsurenceRecommendImage>
+								<InsurenceS.InsurenceImage image="detailView"></InsurenceS.InsurenceImage>
+							</InsurenceS.InsurenceRecommendImage>
+						</HomeS.MainRecommendContentWrapper>,
+					);
+				}
+			}
+			setHanaInsurence(localInsurenceDiv);
 		};
 
 		fetchInsurence();
@@ -27,39 +77,7 @@ function InsurenceComponent() {
 					</HomeS.MainCardTitle>
 				</HomeS.MainCardTitleWrapper>
 				<HomeS.MainRecommendCardWrapper>
-					<HomeS.MainRecommendContentWrapper>
-						<InsurenceS.InsurenceRecommendImage>
-							<InsurenceS.InsurenceImage image="car"></InsurenceS.InsurenceImage>
-						</InsurenceS.InsurenceRecommendImage>
-						<InsurenceS.InsurenceRecommendText>
-							참좋은 운전자 보험
-						</InsurenceS.InsurenceRecommendText>
-						<InsurenceS.InsurenceRecommendImage>
-							<InsurenceS.InsurenceImage image="detailView"></InsurenceS.InsurenceImage>
-						</InsurenceS.InsurenceRecommendImage>
-					</HomeS.MainRecommendContentWrapper>
-					<HomeS.MainRecommendContentWrapper>
-						<InsurenceS.InsurenceRecommendImage>
-							<InsurenceS.InsurenceImage image="travel"></InsurenceS.InsurenceImage>
-						</InsurenceS.InsurenceRecommendImage>
-						<InsurenceS.InsurenceRecommendText>
-							여행자 보험
-						</InsurenceS.InsurenceRecommendText>
-						<InsurenceS.InsurenceRecommendImage>
-							<InsurenceS.InsurenceImage image="detailView"></InsurenceS.InsurenceImage>
-						</InsurenceS.InsurenceRecommendImage>
-					</HomeS.MainRecommendContentWrapper>
-					<HomeS.MainRecommendContentWrapper>
-						<InsurenceS.InsurenceRecommendImage>
-							<InsurenceS.InsurenceImage image="car"></InsurenceS.InsurenceImage>
-						</InsurenceS.InsurenceRecommendImage>
-						<InsurenceS.InsurenceRecommendText>
-							원데이 운전자 보험
-						</InsurenceS.InsurenceRecommendText>
-						<InsurenceS.InsurenceRecommendImage>
-							<InsurenceS.InsurenceImage image="detailView"></InsurenceS.InsurenceImage>
-						</InsurenceS.InsurenceRecommendImage>
-					</HomeS.MainRecommendContentWrapper>
+					{hanaInsurence}
 				</HomeS.MainRecommendCardWrapper>
 			</HomeS.MainCardWrapper>
 		</>
