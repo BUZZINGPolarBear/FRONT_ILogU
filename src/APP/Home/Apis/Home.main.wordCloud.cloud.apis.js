5e2a8ee81b352991c0dfd467147f9538e980a656
Jeonjunhowi <jjh63360@kau.kr>
2023-06-27 17:41:06 +0900

    fix: word cloud console log

--- src/APP/Home/Apis/Home.main.wordCloud.cloud.apis.js
+++ src/APP/Home/Apis/Home.main.wordCloud.cloud.apis.js
@@ -5,8 +5,12 @@ const getChildCloudData = async (keyword) => {
 	try {
 		const response = await axios.get(
 			`${process.env.REACT_APP_ONEHANA_API_SERVER}/related-keyword/child?keyword=${keyword}`,
+			console.log(
+				`request uri : ${process.env.REACT_APP_ONEHANA_API_SERVER}/related-keyword/child?keyword=${keyword}`,
+			),
 		);
-		console.log(keyword);
+		console.log(response);
+
 		return response.data.result;
 	} catch (error) {
 		console.log(`Error: ${error}`);
