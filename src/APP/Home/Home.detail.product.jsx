47099219f88388db5e7a58fd6e8649a4e941b90f
Jeonjunhowi <jjh63360@kau.kr>
2023-06-30 01:02:01 +0900

    add: 상품 상세 설명 페이지 - 설명 wrapper 추가

--- src/APP/Home/Home.detail.product.jsx
+++ src/APP/Home/Home.detail.product.jsx
@@ -13,7 +13,47 @@ function DetailVeiw() {
 	return (
 		<>
 			<DetailS.MainPicWrapper img="/mainScreen/detail/paris.svg"></DetailS.MainPicWrapper>
-			<DetailS.MainScreenWrapper></DetailS.MainScreenWrapper>
+			<DetailS.MainScreenWrapper>
+				<DetailS.MainContentWrapper>
+					<DetailS.MainScreenTitleWrapper>
+						<DetailS.MainScreenTitle type="main">
+							파리여행 26주 적금
+						</DetailS.MainScreenTitle>
+						<DetailS.MainScreenTitle type="sub">
+							아이와 함꼐할 파리여행을 꿈꾸시나요? 하나와 함께하세요!
+						</DetailS.MainScreenTitle>
+					</DetailS.MainScreenTitleWrapper>
+					<DetailS.ContentWrapper>
+						<DetailS.ContentRowWrapper>
+							<DetailS.ContentTitle>상품특징</DetailS.ContentTitle>
+							<DetailS.Content isminimal="false">
+								상품 가입 기간동안 조건 충족 시 우대금리를<br></br> 최고 연 0.5%
+								지급
+							</DetailS.Content>
+						</DetailS.ContentRowWrapper>
+						<DetailS.ContentRowWrapper>
+							<DetailS.ContentTitle>가입대상</DetailS.ContentTitle>
+							<DetailS.Content isminimal="false">개인</DetailS.Content>
+						</DetailS.ContentRowWrapper>
+						<DetailS.ContentRowWrapper>
+							<DetailS.ContentTitle>가입기간</DetailS.ContentTitle>
+							<DetailS.Content isminimal="false">26주</DetailS.Content>
+						</DetailS.ContentRowWrapper>
+						<DetailS.ContentRowWrapper>
+							<DetailS.ContentTitle>가입금액</DetailS.ContentTitle>
+							<DetailS.Content isminimal="false">제한없음</DetailS.Content>
+						</DetailS.ContentRowWrapper>
+						<DetailS.ContentRowWrapper>
+							<DetailS.ContentTitle>가입분류</DetailS.ContentTitle>
+							<DetailS.Content isminimal="false">적금</DetailS.Content>
+						</DetailS.ContentRowWrapper>
+						<DetailS.ContentRowWrapper>
+							<DetailS.ContentTitle>최저가입금액</DetailS.ContentTitle>
+							<DetailS.Content isminimal="true">제한없음</DetailS.Content>
+						</DetailS.ContentRowWrapper>
+					</DetailS.ContentWrapper>
+				</DetailS.MainContentWrapper>
+			</DetailS.MainScreenWrapper>
 		</>
 	);
 }
--- src/APP/Home/Styles/Home.detail.product.styles.js
+++ src/APP/Home/Styles/Home.detail.product.styles.js
@@ -13,20 +13,105 @@ export const MainPicWrapper = styled.div`
 	background-position: center;
 
 	top: 0;
+	z-index: 9999;
 `;
 
 export const MainScreenWrapper = styled.div`
-	position: fixed;
-
+	position: absolute;
+	padding: 5% 5%;
 	top: 40vh;
 	width: 100vw;
 	max-width: 800px;
-	height: 45vh;
+	height: 75vh;
 
 	display: flex;
 	flex-direction: column;
 	align-items: center;
 	justify-content: flex-around;
 
-	background-color: antiquewhite;
+	z-index: 1;
+`;
+
+export const MainContentWrapper = styled.div`
+	width: 100%;
+	height: 55%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: space-around;
+`;
+
+export const MainScreenTitleWrapper = styled.div`
+    width: 100%;
+    height: 10%:
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+`;
+
+export const MainScreenTitle = styled.div`
+	width: 100%;
+	height: fit-content;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: flex-start;
+	${(props) =>
+		props.type == 'main'
+			? `color: ${tokens.colors.grey_700}; font-size: ${tokens.typography.subtitle_1_SB.fontSize}; font-weight: ${tokens.typography.subtitle_1_SB.fontWeight};`
+			: `color: ${tokens.colors.grey_300}; font-size: ${tokens.typography.sub.fontSize}; font-weight: ${tokens.typography.sub.fontWeight};`}
+`;
+
+export const ContentWrapper = styled.div`
+	width: 100%;
+	height: 80%;
+	margin-top: 10px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: space-around;
+`;
+
+export const ContentRowWrapper = styled.div`
+	width: 100%;
+	height: 14%;
+
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: space-between;
+`;
+
+export const ContentTitle = styled.span`
+	width: fit-content;
+	height: fit-content;
+	min-width: 61px;
+
+	padding: 4px 6px;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: center;
+
+	border-radius: 10px;
+
+	background-color: #1dcb9d1a;
+
+	color: ${tokens.colors.green_500};
+	${tokens.typography.caption};
+`;
+
+export const Content = styled.span`
+	width: ${(props) => (props.isminimal == 'false' ? '80%;' : '75%;')}
+	height: 100%;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: flex-start;
+	text-align: left;
+
+	color: ${tokens.colors.grey_700};
+	${tokens.typography.body_2}
 `;
