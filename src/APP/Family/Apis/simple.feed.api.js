185fe2cd0cd702707325a53e90696203e59a87f0
Jeonjunhowi <jjh63360@kau.kr>
2023-06-30 18:09:42 +0900

    add: 피드글 좋아요 추가

--- src/APP/Family/Apis/simple.feed.api.js
+++ src/APP/Family/Apis/simple.feed.api.js
@@ -2,23 +2,18 @@ import axios from 'axios';
 
 export const getSimpleFeed = async (page) => {
 	const token = localStorage.getItem('access');
+
 	try {
-		try {
-			const axiosResponse = await axios.get(
-				`${process.env.REACT_APP_ILOGU_API_SERVER}/api/family/board?page=0&size=${page}&sort=createdAt,desc`,
-				{
-					headers: {
-						Authorization: `Bearer ${token}`,
-					},
+		const axiosResponse = await axios.get(
+			`${process.env.REACT_APP_ILOGU_API_SERVER}/api/family/board?page=0&size=${page}&sort=createdAt,desc`,
+			{
+				headers: {
+					Authorization: `Bearer ${token}`,
 				},
-			);
-			return axiosResponse.data;
-		} catch (error) {
-			return error.response.data.code;
-		}
+			},
+		);
+		return axiosResponse.data;
 	} catch (error) {
-		console.log(`Error is : ${error.response.data.code}`);
-		console.error(error);
 		return error.response.data.code;
 	}
 };
@@ -26,23 +21,41 @@ export const getSimpleFeed = async (page) => {
 export const getFeed = async (page) => {
 	let boardResponseList = [];
 	const token = localStorage.getItem('access');
+
 	try {
-		try {
-			const axiosResponse = await axios.get(
-				`${process.env.REACT_APP_ILOGU_API_SERVER}/api/board?page=0&size=${page}&sort=createdAt,desc`,
-				{
-					headers: {
-						Authorization: `Bearer ${token}`,
-					},
+		const axiosResponse = await axios.get(
+			`${process.env.REACT_APP_ILOGU_API_SERVER}/api/board?page=0&size=${page}&sort=createdAt,desc`,
+			{
+				headers: {
+					Authorization: `Bearer ${token}`,
 				},
-			);
-			return axiosResponse.data;
-		} catch (error) {
-			return error.response.data.code;
-		}
+			},
+		);
+		return axiosResponse.data;
 	} catch (error) {
-		console.log(`Error is : ${error.response.data.code}`);
-		console.error(error);
+		return error.response.data.code;
+	}
+};
+
+export const postLike = async (boardId) => {
+	const token = localStorage.getItem('access');
+	try {
+		console.log(
+			`${process.env.REACT_APP_ILOGU_API_SERVER}/api/board/like/${boardId}`,
+		);
+		const axiosResponse = await axios.put(
+			`${process.env.REACT_APP_ILOGU_API_SERVER}/api/board/like/${boardId}`,
+			{},
+			{
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${token}`,
+				},
+			},
+		);
+
+		return axiosResponse.data;
+	} catch (error) {
 		return error.response.data.code;
 	}
 };
