8e759cb81501da7fd620b90f34c1271d37d9d2b6
Jeonjunhowi <jjh63360@kau.kr>
2023-07-04 15:15:58 +0900

    add: 가족 로그인 시 top nav 없앰

--- src/APP/Family/Family.main.jsx
+++ src/APP/Family/Family.main.jsx
@@ -9,16 +9,24 @@ import {
 } from 'react-router-dom';
 import * as FamilyS from './Styles/Family.main.styles';
 import SimpleFamilyFeed from './Family.main.simple.feed';
+//recoil
 import { useRecoilState, useRecoilValue } from 'recoil';
-import * as utils from '../Feed/getFeed/feed.utils';
 import * as recoilFamily from './recoil/feed.recoil';
+import * as signInRecoil from '../Login/recoil/Login.recoil.states';
+//utils
+import * as utils from '../Feed/getFeed/feed.utils';
+//modal
 import MoneyModal from './Family.main.all.feed.modal.money';
+//api
 import * as api from './Apis/main.feed.api';
 import * as tokenAPI from '../AutoSignIn';
 
 function FamilyMainScreen() {
 	const [isMoneyOpened, setIsMoneyOpened] = useRecoilState(
 		recoilFamily.isMoneyOpend,
+	);
+	const [userFamilyType, setUserFamilyType] = useRecoilState(
+		signInRecoil.familyType,
 	);
 	const [moneyRankDiv, setMoneyRankDiv] = useState([]);
 	const [likeRankDiv, setLikeRankDiv] = useState([]);
@@ -95,85 +103,27 @@ function FamilyMainScreen() {
 			{isMoneyOpened == true ? <MoneyModal type="main"></MoneyModal> : <></>}
 			<FamilyS.TopNavBar>
 				<FamilyS.TopNavTitle>우리가족</FamilyS.TopNavTitle>
-				<FamilyS.TopNavIcon icon="baby" />
-				<FamilyS.TopNavIcon
-					icon="money"
-					onClick={(e) => handleMoneyIconClick(e)}
-				/>
+				{userFamilyType === 'PARENTS' ? (
+					<>
+						<FamilyS.TopNavIcon icon="baby" />
+						<FamilyS.TopNavIcon
+							icon="money"
+							onClick={(e) => handleMoneyIconClick(e)}
+						/>
+					</>
+				) : (
+					<></>
+				)}
 			</FamilyS.TopNavBar>
 			<FamilyS.MainWrapper>
 				<FamilyS.RankWrapper>
 					<FamilyS.MainWrapperTitle>우리 가족 용돈 왕</FamilyS.MainWrapperTitle>
-					<FamilyS.MainRankWrapper>
-						{/* <FamilyS.RankArea>
-							<FamilyS.RankNum type="money">1.</FamilyS.RankNum>
-							<FamilyS.RankTitle>
-								<FamilyS.RankProfilePic url="null"></FamilyS.RankProfilePic>
-								할아버지
-							</FamilyS.RankTitle>
-							<FamilyS.RankContentArea>
-								<FamilyS.RankContent type="money">200,000</FamilyS.RankContent>
-							</FamilyS.RankContentArea>
-						</FamilyS.RankArea>
-						<FamilyS.RankArea>
-							<FamilyS.RankNum type="money">2.</FamilyS.RankNum>
-							<FamilyS.RankTitle>
-								<FamilyS.RankProfilePic url="/Feed/userPic.jpg"></FamilyS.RankProfilePic>
-								삼촌
-							</FamilyS.RankTitle>
-							<FamilyS.RankContentArea>
-								<FamilyS.RankContent type="money">160,000</FamilyS.RankContent>
-							</FamilyS.RankContentArea>
-						</FamilyS.RankArea>
-						<FamilyS.RankArea>
-							<FamilyS.RankNum type="money">3.</FamilyS.RankNum>
-							<FamilyS.RankTitle>
-								<FamilyS.RankProfilePic url="null"></FamilyS.RankProfilePic>
-								이모
-							</FamilyS.RankTitle>
-							<FamilyS.RankContentArea>
-								<FamilyS.RankContent type="money">100,000</FamilyS.RankContent>
-							</FamilyS.RankContentArea>
-						</FamilyS.RankArea> */}
-						{moneyRankDiv}
-					</FamilyS.MainRankWrapper>
+					<FamilyS.MainRankWrapper>{moneyRankDiv}</FamilyS.MainRankWrapper>
 				</FamilyS.RankWrapper>
 
 				<FamilyS.RankWrapper>
 					<FamilyS.MainWrapperTitle>우리 가족 공감 왕</FamilyS.MainWrapperTitle>
-					<FamilyS.MainRankWrapper>
-						{/* <FamilyS.RankArea>
-							<FamilyS.RankNum>1.</FamilyS.RankNum>
-							<FamilyS.RankTitle>
-								<FamilyS.RankProfilePic url="null"></FamilyS.RankProfilePic>
-								할아버지
-							</FamilyS.RankTitle>
-							<FamilyS.RankContentArea>
-								<FamilyS.RankContent type="like">60</FamilyS.RankContent>
-							</FamilyS.RankContentArea>
-						</FamilyS.RankArea>
-						<FamilyS.RankArea>
-							<FamilyS.RankNum>2.</FamilyS.RankNum>
-							<FamilyS.RankTitle>
-								<FamilyS.RankProfilePic url="/Feed/userPic.jpg"></FamilyS.RankProfilePic>
-								삼촌
-							</FamilyS.RankTitle>
-							<FamilyS.RankContentArea>
-								<FamilyS.RankContent type="like">40</FamilyS.RankContent>
-							</FamilyS.RankContentArea>
-						</FamilyS.RankArea>
-						<FamilyS.RankArea>
-							<FamilyS.RankNum type="like">3.</FamilyS.RankNum>
-							<FamilyS.RankTitle>
-								<FamilyS.RankProfilePic url="null"></FamilyS.RankProfilePic>
-								이모
-							</FamilyS.RankTitle>
-							<FamilyS.RankContentArea>
-								<FamilyS.RankContent type="like">37</FamilyS.RankContent>
-							</FamilyS.RankContentArea>
-						</FamilyS.RankArea> */}
-						{likeRankDiv}
-					</FamilyS.MainRankWrapper>
+					<FamilyS.MainRankWrapper>{likeRankDiv}</FamilyS.MainRankWrapper>
 				</FamilyS.RankWrapper>
 				<FamilyS.RankWrapper
 					style={{ height: '50vh' }}
