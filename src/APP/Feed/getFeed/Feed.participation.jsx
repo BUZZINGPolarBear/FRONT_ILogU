48609b4743701496ed6699425a706a0bf2f02390
Jeonjunhowi <jjh63360@kau.kr>
2023-07-04 10:43:33 +0900

    fix: 데이터 - 컴포넌트 렌더링 분리

--- src/APP/Feed/getFeed/Feed.participation.jsx
+++ src/APP/Feed/getFeed/Feed.participation.jsx
@@ -9,21 +9,22 @@ import * as recoil from './recoil/recoild.feed';
 
 function FeedParticipation(props) {
 	const [boardBodyArr, setBoardBodyArr] = useState([]);
+	const [boardBodyContentArr, setBoardBodyContentArr] = useState([]);
 	const [selectedCategory, setSelectedCategory] = useRecoilState(
 		recoil.feedCategoryRecoil,
 	);
+	//게시판 내용 받아오기
 	useEffect(() => {
 		let category = selectedCategory;
-		let boardResponseArr = [];
 
 		if (category == '전체') category = 'ALL';
 		if (category == '여행') category = 'TRAVEL';
 		if (category == '스포츠') category = 'SPORTS';
 		if (category == '일상') category = 'DAILY';
+		let boardResponseArr = [];
 
-		const addBoardDivs = (fetchResponse) => {
-			let localDiv = [];
-			if (fetchResponse.length == 0) setBoardBodyArr(localDiv);
+		const addBoardContentArr = (fetchResponse) => {
+			let apiBoardContent = [];
 			for (let i = 0; i < fetchResponse.length; i++) {
 				const localContent = fetchResponse[i];
 				const dateStr = utils.changeDateStr(localContent.createdAt);
@@ -34,12 +35,63 @@ function FeedParticipation(props) {
 				else if (category === 'SPORTS') category = '⚽️ 스포츠';
 				else if (category === 'TRAVEL') category = '✈️ 여행';
 				else category = '👩‍👩‍👧‍👦 일상';
+				let localContentData = {
+					id: localContent.id,
+					category: category,
+					mainImage: localContent.mainImage.s3url,
+					userProfileUrl: localContent.userProfileUrl,
+					nickname: localContent.nickname,
+					dateStr: dateStr,
+					isLiked: localContent.isLiked,
+					likesCount: localContent.likesCount,
+					commentsCount: localContent.commentsCount,
+					balance: localContent.balance,
+					content: localContent.content,
+					title: localContent.title,
+				};
+				apiBoardContent.push(localContentData);
+			}
+			setBoardBodyContentArr(apiBoardContent);
+		};
 
-				if (
-					localContent.mainImage == null ||
-					typeof localContent.mainImage.s3url == 'undefined'
-				)
-					continue;
+		if (category == 'ALL') {
+			const fetchData = async () => {
+				let getData = await FeedApi.getFeed(category, 30);
+				if (getData == '400-03-04') {
+					await tokenAPI.RefreshToken();
+					getData = await FeedApi.getFeed(category, 30);
+				}
+
+				boardResponseArr = getData.result.content;
+				addBoardContentArr(boardResponseArr);
+			};
+			const fetchResponse = fetchData();
+		} else {
+			const fetchData = async () => {
+				const getData = await FeedApi.getFeed(category, 30);
+				if (getData == '400-03-04') {
+					await tokenAPI.RefreshToken();
+					getData = await FeedApi.getFeed(category, 30);
+				}
+
+				if (getData.result && getData.result.content === undefined) {
+					addBoardContentArr([]);
+				}
+				boardResponseArr = getData.result.content;
+				addBoardContentArr(boardResponseArr);
+			};
+			const fetchResponse = fetchData();
+		}
+	}, [selectedCategory]);
+
+	useEffect(() => {
+		const addBoardDivs = () => {
+			let localDiv = [];
+			for (let i = 0; i < boardBodyContentArr.length; i++) {
+				const localContent = boardBodyContentArr[i];
+				const dateStr = utils.changeDateStr(localContent.createdAt);
+				let category = localContent.category;
+
 				localDiv.push(
 					<FeedparicipateS.FeedChallengeWrapper key={`feed_key${i}`}>
 						<FeedparicipateS.FeedChallengeUserWrapper>
@@ -52,13 +104,13 @@ function FeedParticipation(props) {
 									{localContent.nickname}
 								</FeedparicipateS.FeedChallengeUserInfo>
 								<FeedparicipateS.FeedChallengeUserInfoDate>
-									{dateStr}
+									{localContent.dateStr}
 								</FeedparicipateS.FeedChallengeUserInfoDate>
 							</FeedparicipateS.FeedChallengeUserInfoWrapper>
 						</FeedparicipateS.FeedChallengeUserWrapper>
 
 						<FeedparicipateS.FeedPictureArea
-							picUrl={localContent.mainImage.s3url ?? '/Feed/feed_sample.jpg'}
+							picUrl={localContent.mainImage}
 							alt="사용자"
 						></FeedparicipateS.FeedPictureArea>
 
@@ -91,36 +143,8 @@ function FeedParticipation(props) {
 
 			setBoardBodyArr(localDiv);
 		};
-
-		if (category == 'ALL') {
-			const fetchData = async () => {
-				let getData = await FeedApi.getFeed(category, 30);
-				if (getData == '400-03-04') {
-					await tokenAPI.RefreshToken();
-					getData = await FeedApi.getFeed(category, 30);
-				}
-
-				boardResponseArr = getData.result.content;
-				addBoardDivs(boardResponseArr);
-			};
-			const fetchResponse = fetchData();
-		} else {
-			const fetchData = async () => {
-				const getData = await FeedApi.getFeed(category, 30);
-				if (getData == '400-03-04') {
-					await tokenAPI.RefreshToken();
-					getData = await FeedApi.getFeed(category, 30);
-				}
-
-				if (getData.result && getData.result.content === undefined) {
-					addBoardDivs([]);
-				}
-				boardResponseArr = getData.result.content;
-				addBoardDivs(boardResponseArr);
-			};
-			const fetchResponse = fetchData();
-		}
-	}, [selectedCategory]);
+		addBoardDivs();
+	}, [boardBodyContentArr]);
 	return <>{boardBodyArr}</>;
 }
 
